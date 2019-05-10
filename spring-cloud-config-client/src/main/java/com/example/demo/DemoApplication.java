c1c8339bb8fac537a3ea73114722a39c5fc3d5cd
mphamrh <mpham@redhat.com>
2019-05-09 20:43:08 -0500

    use encryption for configs

--- spring-cloud-config-client/src/main/java/com/example/demo/DemoApplication.java
+++ spring-cloud-config-client/src/main/java/com/example/demo/DemoApplication.java
@@ -3,10 +3,12 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RefreshScope
 @RestController
 public class DemoApplication {
     @Value("${message}")
@@ -18,6 +20,6 @@ public class DemoApplication {
 
     @GetMapping("/message")
     public String sayHello() {
-        return message;
+        return "hello "+message;
     }
 }
